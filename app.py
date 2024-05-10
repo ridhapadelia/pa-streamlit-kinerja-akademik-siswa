c72fba669eecf43eed764db7b857b951f676d1fc
ridhapadelia <126879606+ridhapadelia@users.noreply.github.com>
2024-05-10 22:05:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
